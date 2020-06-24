d4672a8efe6439e1d5445a11738996120400e93f
junho <hojunlee77@gmail.com>
2020-06-24 20:38:41 +0900

    5.1

--- reviews/models.py
+++ reviews/models.py
@@ -11,7 +11,7 @@ class Review(core_models.TimeStampedModel):
     communicatioin = models.IntegerField()
     cleanliness = models.IntegerField()
     location = models.IntegerField()
-    check_in = models.IntegerField()
+    check_in = models.DateField()
     value = models.IntegerField()
     user = models.ForeignKey("users.User", on_delete=models.CASCADE)
     room = models.ForeignKey("rooms.Room", on_delete=models.CASCADE)
